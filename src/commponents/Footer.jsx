de1454f5db93720baac392218542de9c83d28e82
assaf azran <assafaz09@gmail.com>
2025-12-02 13:50:40 +0200

    fix bg

--- src/commponents/Footer.jsx
+++ src/commponents/Footer.jsx
@@ -3,7 +3,7 @@ import React from "react";
 export default function Footer() {
   return (
     <div>
-      <footer className="button bg-black text-white py-6">
+      <footer className="  text-white py-6">
         <div className="container mx-auto text-center">
           <p>&copy; {new Date().getFullYear()} CHAKI. All rights reserved.</p>
         </div>
